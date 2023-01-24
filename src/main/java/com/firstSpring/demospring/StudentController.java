8e44fe8b835b9badc8963af37d62379e923e10fe
unknown <rohini.khandare16598@gmail.com>
2023-01-24 21:46:34 +0530

    initial commit

---
+++ src/main/java/com/firstSpring/demospring/StudentController.java
@@ -0,0 +1,9 @@
+package com.firstSpring.demospring;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController   //first annotation
+public class StudentController {
+
+
+}
